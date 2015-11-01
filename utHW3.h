95b1aa53772fda909df92b2a954da0374cddd100
Vodalok <visualcshape@gmail.com>
2015-11-02 02:34:08 +0800

    Unit test #1 ,#2 ,and #3 finished.

--- utHW3.h
+++ utHW3.h
@@ -7,20 +7,14 @@
 
 #include "GraphicFactory.h"
 #include "cppunitlite/TestHarness.h"
+#include <stack>
+#include <utility>
 
-TEST (GraphicsFactory, fileContentsAsString) {
-    string FILE_NAME = "f1.txt";
-    string EXPECT;
-    EXPECT = "Comp R(-2,-2,3,4)\n"
-                "  Comp R(-2,-2,3,3)\n"
-                "    C(0,0,1)\n"
-                "    S(-2,-2,2)\n"
-                "  R(-1,-1,1,3)";
-    GraphicsFactory graphicsFactory;
-    CHECK(EXPECT == graphicsFactory.fileContentAsString(FILE_NAME.c_str()));
-}
+using std::stack;
+using std::pair;
+using std::make_pair;
 
-TEST(GraphicsFactory, BuildGraphicsFromFile){
+TEST (buildGraphicsFromFileExceptionTest, GraphicsFactory){
     string WRONG_FILE_NAME = "arbitaryFile.txt";
     GraphicsFactory graphicsFactory;
     try{
@@ -31,10 +25,73 @@ TEST(GraphicsFactory, BuildGraphicsFromFile){
     }
 }
 
-TEST(GraphicsFactory, countLevel){
+TEST(buildGraphicsFromFileTest, GraphicsFactory){
+    const char FILE_NAME[] = "f1.txt";
+    string EXPECT;
+    GraphicsFactory factory;
+    EXPECT = "Comp R(-2,-2,3,4)\n"
+            "  Comp R(-2,-2,3,3)\n"
+            "    C(0,0,1)\n"
+            "    S(-2,-2,2)\n"
+            "  R(-1,-1,1,3)\n";
+    DescriptionVisitor descriptionVisitor;
+    Graphics* builtGraphic = factory.buildGraphicsFromFile(FILE_NAME);
+    builtGraphic->accept(descriptionVisitor);
+    CHECK(EXPECT==descriptionVisitor.getDescription());
+}
+
+TEST (fileContentsAsStringfileTest, GraphicsFactory) {
     string FILE_NAME = "f1.txt";
+    string EXPECT;
+    EXPECT = "Comp R(-2,-2,3,4)\n"
+            "  Comp R(-2,-2,3,3)\n"
+            "    C(0,0,1)\n"
+            "    S(-2,-2,2)\n"
+            "  R(-1,-1,1,3)";
     GraphicsFactory graphicsFactory;
-    graphicsFactory.buildGraphicsFromFile(FILE_NAME.c_str());
+    CHECK(EXPECT == graphicsFactory.fileContentAsString(FILE_NAME.c_str()));
+}
+
+TEST(extractGraphicsFromOneLineSimpleGraphicsCircle,GraphicsFactory){
+    const char CIRCLE_LINE[] = "C(1,2,2)\n";
+    int level = 0;
+
+    GraphicsFactory factory;
+    string circleString = CIRCLE_LINE;
+    Graphics* extracted = factory.extractGraphicsFromOneLine(circleString,level);
+    CHECK(extracted->getDescription()==CIRCLE_LINE);
+
+
+}
+
+TEST(extractGraphicsFromOneLineSimpleGraphicsSqure,GraphicsFactory){
+    const char SQUARE_LINE[] = "S(2,2,2)\n";
+    int level = 0;
+    GraphicsFactory factory;
+    string squareString = SQUARE_LINE;
+    Graphics* extracted = factory.extractGraphicsFromOneLine(squareString,level);
+    string des = extracted->getDescription();
+    CHECK(extracted->getDescription()==SQUARE_LINE);
+}
+
+TEST(extractGraphicsFromOneLineCompositeGraphics,GraphicsFactory){
+    const char COMP_LINE[] = "Comp R(0,0,0,0)";
+    int level = 0;
+
+    string compString = COMP_LINE;
+    GraphicsFactory factory;
+    Graphics* extracted = factory.extractGraphicsFromOneLine(compString,level);
+    CHECK(extracted->getDescription() == COMP_LINE);
+}
+
+TEST(composeLine5Test,GraphicsFactory){
+    stack<pair<int,Graphics*>> expectStack;
+    expectStack.push(make_pair(0,new CompositeGraphics));
+    expectStack.push(make_pair(1,new CompositeGraphics));
+    expectStack.push(make_pair(2,new SimpleGraphics(new Circle(0,0,1))));
+    expectStack.push(make_pair(2,new SimpleGraphics(new Square(-2,-2,2))));
+
+
 }
 
 TEST(SelfTest,selfTest){
