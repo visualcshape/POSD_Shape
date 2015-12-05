89f54a8ca80b944427e46748173159e403921367
Vodalok <visualcshape@gmail.com>
2015-12-05 15:21:55 +0800

    Refactor the draw method in SimpleGraphics. Change the l and w parameter in the draw method in composite graphics.

--- SimpleGraphic.cpp
+++ SimpleGraphic.cpp
@@ -43,42 +43,27 @@ void SimpleGraphics::increaseCompositeLevel() {
 }
 
 void SimpleGraphics::draw(QGraphicsScene *scene) {
-    string description =  _shape->describe();
-    string graphicType = "";
-    string graphicAttribute = "";
-    string token = "";
-    const char DELIM = '(';
-    stringstream tokenStringStream(description);
-    std::getline(tokenStringStream, token, DELIM);
-    graphicType = token;
-    Utility::deleteSpace(graphicType);
-    std::getline(tokenStringStream,token, DELIM);
-
-    //remove right brace
-    token.erase(token.length()-1,1);
-    graphicAttribute = token;
-
-    QPen pen(Qt::blue,1,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin);
-    QBrush brush(QColor(0,0,0,0),Qt::SolidPattern);
-    switch(graphicType[0]){
-        case 'R':{
-            Rectangle* pRectangle = SimpleGraphicFactory::createRectangle(graphicAttribute);
-            QRect* rect = new QRect(pRectangle->positionPoint.getX(), pRectangle->positionPoint.getY(), pRectangle->width, pRectangle->height);
-            scene->addRect(*rect,pen,brush);
-        }
-            break;
-        case 'C':{
-            Circle* circle = SimpleGraphicFactory::createCircle(graphicAttribute);
-            QRect* rect = new QRect(circle->centerX - circle->radius, circle->centerY - circle->radius, circle->radius * 2, circle->radius * 2);
-            scene->addEllipse(*rect,pen,brush);
-        }
-            break;
-        case 'S':{
-            Square* square = SimpleGraphicFactory::createSquare(graphicAttribute);
-            QRect* rect = new QRect(square->x, square->y, square->length, square->length);
-            scene->addRect(*rect,pen,brush);
-        }
-            break;
+    //Pen Configuration
+    const QColor PEN_COLOR = Qt::blue;
+    const int PEN_WIDTH = 3;
+    const Qt::PenStyle PEN_STYLE = Qt::SolidLine;
+    const Qt::PenCapStyle PEN_CAP_STYLE = Qt::SquareCap;
+    const Qt::PenJoinStyle PEN_JOIN_STYLE = Qt::RoundJoin;
+    //Brush Configuration
+    const QColor BRUSH_COLOR = QColor(0, 0, 0, 0);
+    const Qt::BrushStyle BRUSH_STYLE = Qt::NoBrush;
+    //Draw on scene
+    QPen pen(PEN_COLOR, PEN_WIDTH, PEN_STYLE, PEN_CAP_STYLE, PEN_JOIN_STYLE);
+    QBrush brush(BRUSH_COLOR, BRUSH_STYLE);
+    BoundingBox shapeBoundingBox = _shape->getBoundingBox();
+    QPoint rectPosition(shapeBoundingBox.llx(), shapeBoundingBox.lly());
+    QSize rectSize(shapeBoundingBox.l(), shapeBoundingBox.w());
+    QRect drawRectangle(rectPosition,rectSize);
+    //Dynamic cast used to decide the concrete shape of shape.
+    Circle* castCircle = dynamic_cast<Circle*>(_shape);
+    if(castCircle != NULL){
+        scene->addEllipse(drawRectangle,pen,brush);
+        return;
     }
-
+    scene->addRect(drawRectangle,pen,brush);
 }
